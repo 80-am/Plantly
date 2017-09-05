c784b29b966d4de4420a2118cb06d10e1ab1a77a
adamberntsson <adam@flonko.com>
2017-09-05 11:02:04 +0200

    added bootstrap

--- src/main/java/com/example/plantly/PlantlyApplication.java
+++ src/main/java/com/example/plantly/PlantlyApplication.java
@@ -4,9 +4,11 @@ import com.example.plantly.Repository.DBRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 @SpringBootApplication
 public class PlantlyApplication {
 
@@ -17,7 +19,7 @@ public class PlantlyApplication {
 		SpringApplication.run(PlantlyApplication.class, args);
 	}
 
-	@GetMapping("")
+	@GetMapping("/")
 	public String homepage() {
 		return "index";
 	}
