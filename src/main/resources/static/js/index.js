6b6da02ee65d9da92d5ccebcc1201ce161b9b497
adamberntsson <adam@flonko.com>
2017-09-11 09:41:21 +0200

    fixed login on about

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -294,6 +294,13 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function(){
+    $("#loginButton").click(function(){
+        $("#form").show();
+        $("#aboutContent").hide();
+    });
+});
+
 $(document).mouseup(function (e)
 {
     var container = $("#form"); // YOUR CONTAINER SELECTOR
@@ -304,4 +311,25 @@ $(document).mouseup(function (e)
         container.fadeOut();
         $("#randomHeader").fadeIn();
     }
+});
+
+
+$(document).mouseup(function (e)
+{
+    var container = $("#form"); // YOUR CONTAINER SELECTOR
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+        $("#aboutContent").fadeIn();
+    }
+});
+
+/* ADD PLANT BUTTON */
+
+$(document).ready(function(){
+    $("#addPlantButton").click(function(){
+        $("#addPlantExtended").show();
+    });
 });
