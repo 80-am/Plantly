f16f8ce5c0bb5f08c31f9b577dec9139394ee93d
adamberntsson <adam@flonko.com>
2017-09-14 15:02:27 +0200

    debugg

--- src/main/java/com/example/plantly/Controller/DBController.java
+++ src/main/java/com/example/plantly/Controller/DBController.java
@@ -109,11 +109,20 @@ public class DBController {
         }
     }
 
+//    @GetMapping("/plantinfo/{plantSpecies}")
+//    @ResponseBody
+//    public ModelAndView plantinfo(@PathVariable String plantSpecies) {
+//        Plant plant = DBConnection.getPlantByPlantSpecies(plantSpecies); // get plant from Plants database using plantSpecies
+//        System.out.println(plant);
+//        return new ModelAndView("plantinfo").addObject("plant", plant);
+//    }
+
     @GetMapping("/plantinfo/{plantSpecies}")
-    public ModelAndView plantinfo(@PathVariable String plantSpecies) {
+    @ResponseBody
+    public Plant plantinfo(@PathVariable String plantSpecies) {
         Plant plant = DBConnection.getPlantByPlantSpecies(plantSpecies); // get plant from Plants database using plantSpecies
         System.out.println(plant);
-        return new ModelAndView("plantinfo").addObject("plant", plant);
+        return plant;
     }
 
     @GetMapping("/addplant")
